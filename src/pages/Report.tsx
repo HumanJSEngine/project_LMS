ab463c54af8ac38777b64f5271a37d0982e53f37
bmj <bmj44571627@gmail.com>
2023-03-17 17:25:57 +0900

    ui 성적조회 작업 중

--- src/pages/Report.tsx
+++ src/pages/Report.tsx
@@ -16,17 +16,19 @@ import {
 
 import ReportBtn from "../components/Report/ReportBtn";
 import ViewReportScore from "../components/Report/ViewReportScore";
+import ReportModal from "../components/Report/ReportModal";
 
 const style = {
   position: "absolute" as "absolute",
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 400,
+  width: 600,
+  height: 600,
   bgcolor: "background.paper",
-  border: "2px solid #000",
-  boxShadow: 24,
-  p: 4,
+  border: "1px solid #000",
+  boxShadow: 20,
+  p: 3,
 };
 
 function createData(name: string, date: string) {
@@ -42,10 +44,6 @@ const rows = [
 ];
 
 export default function BasicTable() {
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-
   return (
     <>
       <TableContainer component={Paper}>
@@ -72,28 +70,13 @@ export default function BasicTable() {
                 </TableCell>
                 <TableCell align="center">{row.date}</TableCell>
                 <TableCell align="right">
-                  <ReportBtn setOpen={handleOpen} />
+                  <ReportModal/>
                 </TableCell>
               </TableRow>
             ))}
           </TableBody>
         </Table>
       </TableContainer>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
-        </Box>
-      </Modal>
     </>
   );
 }

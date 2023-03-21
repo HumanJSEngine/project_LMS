8cffe63a361214ceeb27faec0922687470734efc
dgyoon <douglasyoon9@gmail.com>
2023-03-21 18:10:26 +0900

    Student 페이지 구현
    - Student API 연동 및 리스트 출력

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { RouterProvider } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import { routers } from "./routes";
+import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
+  const queryClient = new QueryClient();
   return (
     <RecoilRoot>
-      <RouterProvider router={routers} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={routers} />
+      </QueryClientProvider>
     </RecoilRoot>
   );
 }
